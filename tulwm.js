4eeb8a56f40664ccee5dc3ee974077a3a5e1aff0
Wiliam Toader <wiliamtoader@protonmail.com>
2026-04-07 05:22:59 +0300

    v0.6 new theme, fixed context menu

--- tulwm.js
+++ tulwm.js
@@ -717,6 +717,7 @@ const TulWM = (function () {
             this.closeBtn.innerHTML = '<svg viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>';
             this.closeBtn.addEventListener('click', () => {
                 this.closeAll();
+                this.destroy();
             });
 
             this.contentAreaEl = Utils.createElement('div', 'tulwm-content-area', this.element);
